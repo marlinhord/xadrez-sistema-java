99d320ff57fc51e4f64ee47225baed2adb94306e
Marlon Araujo <marloncavalcanti1@gmail.com>
2021-11-29 22:41:56 -0300

    [Starting to implement Board and Piece]

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,13 @@
 package application;
 
-import javax.swing.plaf.synth.SynthToggleButtonUI;
-
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Position pos = new Position(3, 5);
-		
-		System.out.println(pos);
+		Board board = new Board(8, 8);
 
 	}
 
