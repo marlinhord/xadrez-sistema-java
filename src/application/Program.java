bdfa5fae8966705b273c690cb9862ffb9505670a
Marlon Araujo <marloncavalcanti1@gmail.com>
2021-11-29 22:32:13 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,16 @@
 package application;
 
+import javax.swing.plaf.synth.SynthToggleButtonUI;
+
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello world");
+		Position pos = new Position(3, 5);
+		
+		System.out.println(pos);
 
 	}
 
